0ff04b982d8379cc7d2507cf07e3d4c6339c9375
utzmankazeem <usman4kay@gmail.com>
2024-12-19 11:28:28 +0100

    edit the file routes and links

---
+++ config/dbConn.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config()
+import mongoose from 'mongoose';
+const isProduction = process.env.NODE_ENV === 'production'
+const dbUri = isProduction ? process.env.DATABASE_URI : process.env.DATABASE_URI_LOCAL
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            family: 4
+        })
+    } catch (error) {
+        throw error
+    }
+}
+export default connectDB;
