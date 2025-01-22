38956e373c8ed6df4d110f0b6384b174d5f8a979
utzmankazeem <usman4kay@gmail.com>
2025-01-22 17:45:39 +0100

    fix: design issues in edit page, feat: update dbconn with adv logic, removing the deprecated components

--- config/dbConn.js
+++ config/dbConn.js
@@ -5,9 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 const dbUri = isProduction ? process.env.DATABASE_URI : process.env.DATABASE_URI_LOCAL
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
+        await mongoose.connect(dbUri, {
             family: 4
         })
     } catch (error) {
